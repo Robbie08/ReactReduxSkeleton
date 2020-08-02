b1b897184fa8f88a5ae97e4454cbcf98143fb298
Robbie08 <08ortiz.roberto@gmail.com>
2020-08-02 15:28:53 -0700

    Push Skeleton of Project, Basic Redux Application

--- src/index.js
+++ src/index.js
@@ -3,6 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'; // import redux
+
+
+// create our reducer - here we handle our actions
+// @param1 : state
+// @param2 : action
+const mainReducer = (state, action) => {
+  switch(action.type){
+    case "ADD":
+      state += action.payload; // adds the value passed as payload
+      break;
+    case "SUBTRACT":
+      state -= action.payload; // subtracts the value passed as payload
+      break;
+    default:
+      break;
+  }
+
+  return state;
+}
+
+// This is how we create the redux store. make sure to import {createStore}
+// @param1 : reducer
+// @param2 : initialState - can be object or whatever
+const store = createStore(mainReducer,1);  
+
+// we will subscribe and print out whenever the store notices a change in state
+store.subscribe(() => {
+  console.log("Store Updated!", store.getState());
+});
+
+// Dispatch our action
+store.dispatch({
+  type: "ADD",
+  payload: 10,
+});
+
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 10,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
