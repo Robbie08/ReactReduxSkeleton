780115a1af28895857d24edd8ab392142f5cadc3
Robbie08 <08ortiz.roberto@gmail.com>
2020-08-02 16:21:28 -0700

    Incorporated a State as an object in our Redux Skeleton

--- src/index.js
+++ src/index.js
@@ -6,16 +6,36 @@ import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'; // import redux
 
 
+const initialState = {
+  result: 10,
+  lastValues: [],
+  userName: "Robert"
+}
+
 // create our reducer - here we handle our actions
-// @param1 : state
+// @param1 : state (if no state gets passed in the we will use initialState)
 // @param2 : action
-const mainReducer = (state, action) => {
+const mainReducer = (state = initialState, action) => {
   switch(action.type){
     case "ADD":
-      state += action.payload; // adds the value passed as payload
+      // if we want to update the initialState we can create a new object
+      // then use the spread op to destructure our data back into the new object
+      // finally we will override the first declaration by updating the state
+      state ={
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload],
+      };
       break;
     case "SUBTRACT":
-      state -= action.payload; // subtracts the value passed as payload
+      // if we want to update the initialState we can create a new object
+      // then use the spread op to destructure our data back into the new object
+      // finally we will override the first declaration by updating the state
+      state ={
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload],
+      }
       break;
     default:
       break;
@@ -27,14 +47,14 @@ const mainReducer = (state, action) => {
 // This is how we create the redux store. make sure to import {createStore}
 // @param1 : reducer
 // @param2 : initialState - can be object or whatever
-const store = createStore(mainReducer,1);  
+const store = createStore(mainReducer,initialState);  
 
 // we will subscribe and print out whenever the store notices a change in state
 store.subscribe(() => {
   console.log("Store Updated!", store.getState());
 });
 
-// Dispatch our action
+// Dispatch our actions
 store.dispatch({
   type: "ADD",
   payload: 10,
