281290f77e33b8bfa181032d9e754d304df62ede
Robbie08 <08ortiz.roberto@gmail.com>
2020-08-03 18:44:38 -0700

    Incorporated Multi-Reducer/State with combineReducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Hello world!</h1>
+          <h1>Hello World!</h1>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux'; // import redux
+import {createStore, combineReducers, applyMiddleware} from 'redux'; // import redux
 
 
-const initialState = {
+const mathInitState = {
   result: 10,
   lastValues: [],
-  userName: "Robert"
+};
+
+const userInitState = {
+  userName: "PimpOG94",
+  userAge: 23,
 }
 
 // create our reducer - here we handle our actions
 // @param1 : state (if no state gets passed in the we will use initialState)
 // @param2 : action
-const mainReducer = (state = initialState, action) => {
+const mathReducer = (state = mathInitState, action) => {
   switch(action.type){
     case "ADD":
       // if we want to update the initialState we can create a new object
@@ -37,6 +41,7 @@ const mainReducer = (state = initialState, action) => {
         lastValues: [...state.lastValues, action.payload],
       }
       break;
+
     default:
       break;
   }
@@ -44,17 +49,67 @@ const mainReducer = (state = initialState, action) => {
   return state;
 }
 
+
+// creater another reducer to help handle state changes of the user
+const userReducer = (state = userInitState, action) => {
+
+  switch(action.type){
+    case "SET_NAME":
+      state = {
+        ...state, // copy our original state in the new state
+        userName: action.payload, 
+      };
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        userAge: action.payload,
+      };
+      break;
+
+    default:
+      break;
+  }
+  return state;
+}
+
+// we can think of this object as one larger global state with 
+// multiple substates within it
+// this is how we can combine our projects into one larger app
+const mainReducer = combineReducers({
+  mathReducer,
+  userReducer,
+});
+
+// create middleware for that allows out reducers to communicate
+const myLogger = (store) => (next) => (action) => {
+  console.log("Logged Action: ", action);
+  next(action); // this will allow our action to travel to our reducer
+  
+}
+
 // This is how we create the redux store. make sure to import {createStore}
 // @param1 : reducer
 // @param2 : initialState - can be object or whatever
-const store = createStore(mainReducer,initialState);  
+const store = createStore(mainReducer, {}, applyMiddleware());  
 
 // we will subscribe and print out whenever the store notices a change in state
 store.subscribe(() => {
   console.log("Store Updated!", store.getState());
 });
 
-// Dispatch our actions
+// Dispatch our userName actions
+store.dispatch({
+  type: "SET_NAME",
+  payload: "Bubba98",
+})
+
+store.dispatch({
+  type:"SET_AGE",
+  payload: 25,
+})
+
+// Dispatch our math actions
 store.dispatch({
   type: "ADD",
   payload: 10,
@@ -68,7 +123,7 @@ store.dispatch({
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
